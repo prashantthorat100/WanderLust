02f3fc86d8c0673ce2d611073bb6bb5b267abc46
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-29 22:32:46 +0530

    Navbar Added

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate")
 
+
 const app = express();
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname ,"/public/css")))
 
 app.get("/", (req, res) => {
   res.send("Root Directory");
